5f79f4c48abdcb5aafa07cf0093578a3838bb8fe
HENRIQUE <henrique_holtz@hotmail.com>
2021-02-17 00:01:59 -0300

    Created project react-app with only home message

---
+++ pomocontrol/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Welcome to Home of PomoControl</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
