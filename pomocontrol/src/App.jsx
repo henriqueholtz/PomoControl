81b7f50f85a4b98e7e7b740fd78943a5d67240af
henriqueholtz <henrique_holtz@hotmail.com>
2021-07-05 23:56:12 -0300

    Add react-router-dom with home and test only

--- pomocontrol/src/App.jsx
+++ pomocontrol/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter , Route, Switch } from 'react-router-dom'
 
 export function App() {
   return (
     <div className="App">
-      <h1>Teste</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={["", "/", "/home"]}>
+            <h1>Home</h1>
+          </Route>
+          <Route path="/test">
+            <h1>Test</h1>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
