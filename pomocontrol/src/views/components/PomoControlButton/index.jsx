9767414fd1687f945900e4053bb6d05f3872cb9a
henriqueholtz <henrique_holtz@hotmail.com>
2021-07-06 22:22:23 -0300

    Add prop-types

--- pomocontrol/src/views/components/PomoControlButton/index.jsx
+++ pomocontrol/src/views/components/PomoControlButton/index.jsx
@@ -1,4 +1,14 @@
+import PropTypes from 'prop-types'
+
 export function PomoControlButton({text}) {
 
     return <button type="button">{text}</button>
+}
+
+PomoControlButton.defaultProps = {
+
+}
+
+PomoControlButton.propTypes = {
+    text: PropTypes.string.isRequired
 }
--- pomocontrol/src/views/components/PomoControlHeader/index.jsx
+++ pomocontrol/src/views/components/PomoControlHeader/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export function PomoControlHeader({title}) {
     return <header>
             <h1>{title}</h1>
         </header>
+}
+
+PomoControlHeader.defaultProps = {
+    
+}
+
+PomoControlHeader.propTypes = {
+    title: PropTypes.string.isRequired
 }
