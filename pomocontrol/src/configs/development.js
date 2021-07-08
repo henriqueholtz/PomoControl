d815be5f392a94bbf13cc2df359f030f3124ce02
henriqueholtz <henrique_holtz@hotmail.com>
2021-07-07 22:35:26 -0300

    Add DesenvConfig.js

---
+++ pomocontrol/src/configs/development.js
@@ -0,0 +1,10 @@
+import { EnumEnvironment } from '@pomocontrol-enums'
+
+const config = {
+    api: {
+        backendUrl: 'https://localhost:1000/api/'
+    },
+    environment: EnumEnvironment.DEBUG
+}
+
+export const DesenvConfig = config;
---
+++ pomocontrol/src/configs/index.js
@@ -0,0 +1,1 @@
+export { DesenvConfig } from './development'
