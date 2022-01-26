4e4868456cbd5621e0ca77f44303cbd514b38e50
henriqueholtz <henrique_holtz@hotmail.com>
2022-01-26 05:37:38 -0300

    Using PomoControlProviderToken

--- pomocontrol/src/index.js
+++ pomocontrol/src/index.js
@@ -1,16 +1,19 @@
-import { createBrowserHistory } from 'history'
+import { createBrowserHistory } from 'history';
 import React from 'react';
+import { PomoControlProviderToken } from '@pomocontrol-contexts';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom'
-import { App }  from './routes';
+import { Router } from 'react-router-dom';
+import { App } from './routes';
 
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router history={history}>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Router history={history}>
+            <PomoControlProviderToken>
+                <App />
+            </PomoControlProviderToken>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
