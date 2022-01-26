1cdc26096a243bd9e69bde50d28801ad82c61d36
henriqueholtz <henrique_holtz@hotmail.com>
2022-01-26 07:37:11 -0300

    Let PomoControlMenu dynamic (with Array by props)

--- pomocontrol/src/views/components/PomoControlMenu/Menus.js
+++
@@ -1,3 +0,0 @@
-export const ListMenuAnonymous = ['dashboard', 'login', 'register', 'technical', 'theme', 'NotFound', 'Logout'];
-export const ListMenuAuthenticated = ['dashboard', 'about', 'profile'];
-export const ListMenuAdm = ['dashboard', 'about', 'profile', 'panel'];
--- pomocontrol/src/views/components/PomoControlMenu/index.jsx
+++ pomocontrol/src/views/components/PomoControlMenu/index.jsx
@@ -1,27 +1,21 @@
 import { AppBar } from '@material-ui/core';
-import React from 'react'
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { PomoControlMenuItem } from './PomoControlMenuItem';
-import { ListMenuAnonymous } from './Menus'
 import styles from './styles.module.scss';
-// import { ListMenuAnonymous, ListMenuAuthenticated, ListMenuAdm } from './Menus'
- 
-export function PomoControlMenu() {
-    // const [menu, setMenu] = useState();
 
+export function PomoControlMenu({ menu }) {
     return (
         <AppBar className={styles.appBar}>
-            {ListMenuAnonymous.map((menuItem) => 
-                <PomoControlMenuItem id={menuItem} text={menuItem} path={`/${menuItem}`} />
-            )}
+            {menu?.map((menuItem) => (
+                <PomoControlMenuItem key={menuItem} id={menuItem} text={menuItem} path={`/${menuItem}`} />
+            ))}
         </AppBar>
-    )
+    );
 }
 
-PomoControlMenu.defaultProps = {
-
-}
+PomoControlMenu.defaultProps = {};
 
 PomoControlMenu.propTypes = {
-
-}
+    menu: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
