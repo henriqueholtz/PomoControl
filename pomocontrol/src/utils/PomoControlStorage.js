b722be3bccae7e79eb37afc8d73abd482177b7ff
henriqueholtz <henrique_holtz@hotmail.com>
2022-01-27 04:54:33 -0300

    create PomoControlStorage

--- pomocontrol/src/utils/PomoControlStorage.js
+++ pomocontrol/src/utils/PomoControlStorage.js
@@ -1,26 +1,31 @@
 import Cookie from 'js-cookie';
 
-export const PomoControlStorage = () => {
-    const storage = {};
-    try {
-        if (!window.localStorage) {
-            throw Error('no local Storage');
-        }
-    
-        storage.set = (key, value) => localStorage.setItem(key, JSON.stringify(value));
-        storage.get = (key) => {
+export class PomoControlStorage {
+    static Get(key) {
+        try {
             const item = localStorage.getItem(key);
-            JSON.parse(item)
+            if (!item) return null;
+            return JSON.parse(item)
                 .then((result) => result)
-                .catch(() => null)
+                .catch(() => null);
+        } catch (ex) {
+            return Cookie.get(key);
         }
-    
-        storage.remove = key => localStorage.removeItem(key);
     }
-    catch (ex) {
-        storage.set = Cookie.set;
-        storage.get = Cookie.get;
-        storage.remove = Cookie.remove;
+
+    static Set(key, value) {
+        try {
+            localStorage.setItem(key, JSON.stringify(value));
+        } catch (ex) {
+            Cookie.set(key, value);
+        }
     }
-    return storage;
+
+    static Remove(key) {
+        try {
+            localStorage.removeItem(key);
+        } catch (ex) {
+            Cookie.remove(key);
+        }
+    }
 }
