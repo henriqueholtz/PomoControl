ae9d0c6b335c6858bac9918e32c06298ff77ce7b
henriqueholtz <henrique_holtz@hotmail.com>
2022-01-27 03:50:32 -0300

    Populate About page

--- pomocontrol/src/views/pages/About/index.jsx
+++ pomocontrol/src/views/pages/About/index.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import { PomoControlHeader } from '@pomocontrol-components'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { PomoControlHeader } from '@pomocontrol-components';
+import { Container, Typography } from '@material-ui/core';
 
 export function About() {
     return (
         <>
-            <PomoControlHeader title="About"/>
+            <Container style={{ marginTop: '10%', paddingLeft: '25%' }}>
+                <PomoControlHeader title="About PomoControl" />
+                <Typography style={{ marginTop: '15px' }}>
+                    {`This is a open source project to control your Pomodoro. `}
+                    <Link to="/Pomodoro">See more here</Link>
+                </Typography>
+                <Typography style={{ marginTop: '15px' }}>
+                    {`See the GitHub repository from frontend `}
+                    <a href="https://github.com/henriqueholtz/PomoControl" target="_blank" rel="noreferrer noopener" noope>
+                        here
+                    </a>
+                </Typography>
+                <Typography style={{ marginTop: '15px' }}>
+                    {`See also the GitHub repository from backend `}
+                    <a href="https://github.com/henriqueholtz/PomoControl-backend" target="_blank" rel="noreferrer noopener" noope>
+                        here
+                    </a>
+                </Typography>
+            </Container>
         </>
-    )
+    );
 }
