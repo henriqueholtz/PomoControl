c293f71b409d6099937fa10359a3b4179c31b6bf
henriqueholtz <henrique_holtz@hotmail.com>
2022-01-27 04:58:45 -0300

    Fix propTypes to PrivateCustomRoute

--- pomocontrol/src/routes/PrivateCustomRoute.jsx
+++ pomocontrol/src/routes/PrivateCustomRoute.jsx
@@ -1,19 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 import { PomoControlStoreContextToken } from '@pomocontrol-contexts';
 import { PrivateLayout } from '@pomocontrol-layouts';
 
-export function PrivateCustomRoute({ component: Component, ...rest }) {
+export function PrivateCustomRoute({ ...rest }) {
     // const user = useSelector(state => state && state.oidc  && state.oidc.user)
     const { token } = useContext(PomoControlStoreContextToken);
-    console.log('PrivateCustomRoute', token);
     return <PrivateLayout>{token ? <Route {...rest} /> : <Redirect to="/login" />}</PrivateLayout>;
 }
 
 PrivateCustomRoute.defaultProps = {};
 
-PrivateCustomRoute.propTypes = {
-    component: PropTypes.node.isRequired,
-};
+PrivateCustomRoute.propTypes = {};
